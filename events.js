d04e54604dc5522711d8d7b167a45f8160f707f1
Amiir25 <amirsadik636@gmail.com>
2025-05-13 21:44:16 +0300

    Update events.js

--- events.js
+++ events.js
@@ -9,6 +9,7 @@ const events = [
         "address": "Millennium Hall, Bole, Addis Ababa",
         "price": "250 ETB",
         "spots": 300,
+        "spotplus": '',
         "age": 16,
         "image": "./img/tech-expo.webp"
     },
@@ -22,22 +23,350 @@ const events = [
         "address": "UNECA Conference Center, Menelik II Ave, Addis Ababa",
         "price": "Free",
         "spots": 300,
+        "spotplus": '',
         "age": 18,
-        "image": "./img/civil-service-reform.jpeg"
+        "image": "./img/civil-service-reform.webp"
     },
-    // {
-    //     "id": 3,
-    //     "title": "Children’s Storytelling & Puppet Show",
-    //     "description": "Fun and interactive storytelling session with Ethiopian folk tales, puppets, and crafts.",
-    //     "date": "May 14, 2025",
-    //     "address": "Entoto Park Family Zone, Addis Ababa",
-    //     "price": "60 ETB",
-    //     "spots": 80,
-    //     "age": "4+",
-    //     "image": "./img/children-puppet-show.png"
-    // },
+    {
+        "id": 3,
+        "title": "Children’s Storytelling & Puppet Show",
+        "description": "Fun and interactive storytelling session with Ethiopian folk tales, puppets, and crafts.",
+        "date": "May 14, 2025",
+        "datetime": "2025-05-14",
+        "remainingdays": 2,
+        "address": "Entoto Park Family Zone, Addis Ababa",
+        "price": "60 ETB",
+        "spots": 20,
+        "spotplus": '',
+        "age": 4,
+        "image": "./img/children-puppet-show.webp"
+    },
+    {
+        "id": 4,
+        "title": "Arba Minch Green Cities Dialogue",
+        "description": "Government, NGO, and community leaders discuss urban sustainability and water conservation.",
+        "date": "May 26, 2025",
+        "datetime": "2025-05-26",
+        "remainingdays": 14,
+        "address": "Haile Resort Conference Hall, Arba Minch",
+        "price": "Free",
+        "spots": 120,
+        "spotplus": '',
+        "age": 18,
+        "image": "./img/green-cities-dialog.webp"
+    },
+    {
+        "id": 5,
+        "title": "Coding for Kids Bootcamp",
+        "description": "Introductory workshop in Scratch and robotics for kids aged 7–14.",
+        "date": "June 28, 2025",
+        "datetime": "2025-06-28",
+        "remainingdays": 46,
+        "address": "IceAddis Innovation Hub, Lideta",
+        "price": "150 ETB",
+        "spots": 3,
+        "spotplus": '',
+        "age": 7,
+        "image": "./img/coding-for-kids.webp"
+    },
+    {
+        "id": 6,
+        "title": "Cultural Heritage Policy Forum",
+        "description": "Academics and policymakers gather to debate cultural preservation and access.",
+        "date": "May 16, 2025",
+        "datetime": "2025-05-16",
+        "remainingdays": 4,
+        "address": "Institute of Ethiopian Studies, Sidist Kilo",
+        "price": "120 ETB",
+        "spots": 100,
+        "spotplus": '',
+        "age": 18,
+        "image": "./img/cultural-heritage-forum.webp"
+    },
+    {
+        "id": 7,
+        "title": "Parenting Skills Workshop",
+        "description": "A hands-on workshop for parents on positive discipline, communication, and emotional development.",
+        "date": "May 14, 2025",
+        "datetime": "2025-05-14",
+        "remainingdays": 2,
+        "address": "Addis Ababa City Library, Arat Kilo",
+        "price": "100 ETB",
+        "spots": 10,
+        "spotplus": '',
+        "age": 21,
+        "image": "./img/parenting-workshop.webp"
+    },
+    {
+        "id": 8,
+        "title": "Agricultural Innovation Field Day",
+        "description": "On-site demo of new irrigation, crop health, and climate-smart techniques.",
+        "date": "May 18, 2025",
+        "datetime": "2025-05-18",
+        "remainingdays": 6,
+        "address": "Melkassa Agricultural Research Center, Adama",
+        "price": "80 ETB",
+        "spots": 60,
+        "spotplus": '',
+        "age": 16,
+        "image": "./img/agricultural-innovation.webp"
+    },
+    {
+        "id": 9,
+        "title": "Mental Health Awareness Seminar",
+        "description": "Doctors and advocates raise awareness on anxiety, depression, and mental resilience.",
+        "date": "May 12, 2025",
+        "datetime": "2025-05-12",
+        "remainingdays": 0,
+        "address": "Tikur Anbessa Specialized Hospital Auditorium, Addis Ababa",
+        "price": "Free",
+        "spots": 100,
+        "spotplus": '',
+        "age": 18,
+        "image": "./img/mental-health-seminar.webp"
+    },
+    {
+        "id": 10,
+        "title": "Women in Business Leadership Forum",
+        "description": "Connect with Ethiopia’s top women leaders in entrepreneurship and corporate innovation.",
+        "date": "June 13, 2025",
+        "datetime": "2025-06-13",
+        "remainingdays": 31,
+        "address": "Sheraton Addis Hotel, Addis Ababa",
+        "price": "300 ETB",
+        "spots": 200,
+        "spotplus": '',
+        "age": 18,
+        "image": "./img/women-business-forum.webp"
+    },
+    {
+        "id": 11,
+        "title": "Lalibela Heritage Tour & Workshop",
+        "description": "Guided tour of Lalibela’s rock-hewn churches followed by a workshop on Ethiopian Orthodox art and history",
+        "date": "May 15, 2025",
+        "datetime": "2025-05-15",
+        "remainingdays": 3,
+        "address": "Lalibela World Heritage Site, Lalibela",
+        "price": "800 ETB",
+        "spots": 40,
+        "spotplus": '',
+        "age": 10,
+        "image": "./img/lalibela-heritage-workshop.webp"
+    },
+    {
+        "id": 12,
+        "title": "Coffee Ceremony & Traditional Storytelling",
+        "description": "Experience an authentic Ethiopian coffee ceremony while listening to folklore tales from local elders.",
+        "date": "May 13, 2025",
+        "datetime": "2025-05-13",
+        "remainingdays": 1,
+        "address": "Kaldi’s Coffee Cultural Center, Bole Road, Addis Ababa",
+        "price": "350 ETB",
+        "spots": 7,
+        "spotplus": '',
+        "age": 0,
+        "image": "./img/coffe-ceremony.webp"
+    },
+    {
+        "id": 13,
+        "title": "Startup Pitch Competition",
+        "description": "Entrepreneurs present their business ideas to a panel of investors. Winners receive seed funding.",
+        "date": "June 30, 2025",
+        "datetime": "2025-06-30",
+        "remainingdays": 48,
+        "address": "Blue Moon Hotel, Kazanchis, Addis Ababa",
+        "price": "Free to attend (500 ETB to pitch)",
+        "spots": 150,
+        "spotplus": "(20 to pitch)",
+        "age": 18,
+        "image": "./img/startup-competition.webp"
+    },
+    {
+        "id": 14,
+        "title": "Traditional Ethiopian Foods Cooking Class",
+        "description": "Learn to cook injera, doro wat, and shiro from a professional chef. Ingredients and recipes provided.",
+        "date": "June 3, 2025",
+        "datetime": "2025-06-3",
+        "remainingdays": 21,
+        "address": "Yod Abyssinia Cultural Restaurant, Bole, Addis Ababa",
+        "price": "1200 ETB",
+        "spots": 15,
+        "spotplus": "",
+        "age": 12,
+        "image": "./img/ethiopan-food-cooking.webp"
+    },
+    {
+        "id": 15,
+        "title": "Public Health Awareness Seminar",
+        "description": "Government health officials discuss disease prevention, vaccination, and wellness programs.",
+        "date": "May 18, 2025",
+        "datetime": "2025-05-18",
+        "remainingdays": 6,
+        "address": "Ethiopian Public Health Institute, Gulele, Addis Ababa",
+        "price": "Free",
+        "spots": 250,
+        "spotplus": "",
+        "age": 0,
+        "image": "./img/public-health.webp"
+    },
+    {
+        "id": 16,
+        "title": "Adventure Hike to Entoto Mountain",
+        "description": "A guided nature hike with stunning views of Addis Ababa. Includes a short history lesson on Entoto’s significance.",
+        "date": "May 22, 2025",
+        "datetime": "2025-05-22",
+        "remainingdays": 10,
+        "address": "Entoto Natural Park, Addis Ababa",
+        "price": "400 ETB (Includes water & guide)",
+        "spots": 40,
+        "spotplus": "",
+        "age": 10,
+        "image": "./img/entoto-mountain.webp"
+    },
+    {
+        "id": 17,
+        "title": "Children’s Art & Craft Festival",
+        "description": "A fun-filled day of painting, pottery, and creative workshops for kids. Prizes for best artwork!",
+        "date": "May 13, 2025",
+        "datetime": "2025-05-13",
+        "remainingdays": 1,
+        "address": "National Museum of Ethiopia, Addis Ababa",
+        "price": "300 ETB",
+        "spots": 80,
+        "spotplus": "",
+        "age": 4,
+        "image": "./img/children-art.webp"
+    },
+    {
+        "id": 18,
+        "title": "Comedy Night: Stand-Up Showcase",
+        "description": "An evening of laughter with Ethiopia’s top comedians delivering fresh, local humor in English and Amharic",
+        "date": "May 16, 2025",
+        "datetime": "2025-05-16",
+        "remainingdays": 4,
+        "address": "Hager Fikir Theatre, Piazza, Addis Ababa",
+        "price": "500 ETB",
+        "spots": 120,
+        "spotplus": "",
+        "age": 16,
+        "image": "./img/comedy-night.webp"
+    },
+    {
+        "id": 19,
+        "title": "Public Lecture: “The Future of AI in Africa",
+        "description": " A thought-provoking lecture on how artificial intelligence can transform healthcare, education, and agriculture in Africa",
+        "date": "May 12, 2025",
+        "datetime": "2025-05-12",
+        "remainingdays": 0,
+        "address": "Addis Ababa Science Museum Auditorium, Mexico Square",
+        "price": "Free",
+        "spots": 100,
+        "spotplus": "",
+        "age": 16,
+        "image": "./img/future-of-ai.webp"
+    },
+    {
+        "id": 20,
+        "title": "Chess for Change: Open Tournament",
+        "description": "Join players from all backgrounds in this open chess tournament for all skill levels. Trophies and mentorships awarded",
+        "date": "May 20, 2025",
+        "datetime": "2025-05-20",
+        "remainingdays": 8,
+        "address": "Yeka Sub-City Youth Center, Addis Ababa",
+        "price": "30 ETB",
+        "spots": 50,
+        "spotplus": "",
+        "age": 10,
+        "image": "./img/chess-competition.webp"
+    },
+    {
+        "id": 21,
+        "title": "DIY Home Decor Workshop",
+        "description": "Learn how to craft home decorations using recyclable materials and traditional Ethiopian crafts.",
+        "date": "May 16, 2025",
+        "datetime": "2025-05-16",
+        "remainingdays": 4,
+        "address": "Zoma Museum Garden Workshop, Addis Ababa",
+        "price": "100 ETB",
+        "spots": 30,
+        "spotplus": "",
+        "age": 14,
+        "image": "./img/home-decore.webp"
+    },
+    {
+        "id": 22,
+        "title": "Street Photography Contest & Walk",
+        "description": "Capture the essence of Addis through your lens. Submit your best shots for a chance to win prizes and exhibitions.",
+        "date": "May 14, 2025",
+        "datetime": "2025-05-14",
+        "remainingdays": 2,
+        "address": "Starting Point: St. George Cathedral, Addis Ababa",
+        "price": "50 ETB",
+        "spots": 60,
+        "spotplus": "",
+        "age": 13,
+        "image": "./img/street-photography.webp"
+    },
+    {
+        "id": 23,
+        "title": "Health & Wellness Fair",
+        "description": "Attend yoga sessions, health checkups, and wellness talks from local experts and traditional healers.",
+        "date": "May 13, 2025",
+        "datetime": "2025-05-13",
+        "remainingdays": 1,
+        "address": "Gullele Botanic Garden Pavilion, Addis Ababa",
+        "price": "60 ETB",
+        "spots": 150,
+        "spotplus": "",
+        "age": 12,
+        "image": "./img/health-and-wellness.webp"
+    },
+    {
+        "id": 24,
+        "title": "Social Impact Film Screening & Panel",
+        "description": "Watch short films tackling social issues in Ethiopia followed by a panel of filmmakers and activists.",
+        "date": "May 12, 2025",
+        "datetime": "2025-05-12",
+        "remainingdays": 0,
+        "address": "Alliance Ethio-Française, Addis Ababa",
+        "price": "80 ETB",
+        "spots": 100,
+        "spotplus": "",
+        "age": 18,
+        "image": "./img/short-films.webp"
+    },
+    {
+        "id": 25,
+        "title": "Ethiopian Space Exploration Workshop",
+        "description": "A seminar showcasing Ethiopia’s space initiatives and satellite technology",
+        "date": "May 15, 2025",
+        "datetime": "2025-05-15",
+        "remainingdays": 3,
+        "address": "Ethiopian Space Science Society, Addis Ababa",
+        "price": "300 ETB",
+        "spots": 100,
+        "spotplus": "",
+        "age": 16,
+        "image": "./img/ethiopian-space-science.webp"
+    },
+    {
+        "id": 26,
+        "title": "Gaming & Esports Championship",
+        "description": "A competitive gaming event featuring tournaments, meet-ups, and tech showcases.",
+        "date": "May 18, 2025",
+        "datetime": "2025-05-18",
+        "remainingdays": 6,
+        "address": "Century Mall, Addis Ababa",
+        "price": "350 ETB",
+        "spots": 300,
+        "spotplus": "",
+        "age": 13,
+        "image": "./img/gaming-championship.webp"
+    },
     // More events...
 ];
+
+
 
 /* Event - 1
 <!-- Event 1 -->
@@ -68,6 +397,7 @@ const events = [
                             <span class="event-price">250 ETB</span>
                         </p>
                         <!-- Available Spots -->
+                        "spotplus": '',
                         <p class="mb-2">
                             <!-- <i class="fa-solid fa-chair mr-2"></i> -->
                             <span class="mr-2">👥</span>
@@ -93,7 +423,7 @@ const events = [
 /* Event - 2
 < !--Event 2 -- >
 <div class="max-w-xs md:max-w-sm rounded overflow-hidden shadow-2xl m-auto mt-5">
-    <img class="w-full" src="images/civil-service-reform.jpeg" alt="Sunset in the mountains">
+    <img class="w-full" src="images/civil-service-reform.webp" alt="Sunset in the mountains">
     <div class="px-6 py-4">
         <!-- Title -->
         <div class="event-title font-bold text-xl mb-2">National Civil Service Reform Summit</div>
@@ -144,7 +474,7 @@ const events = [
 /* Event - 3 
 < !--Event 3 -- >
 <div class="max-w-xs md:max-w-sm rounded overflow-hidden shadow-2xl m-auto mt-5">
-    <img class="w-full" src="images/children-puppet-show.png" alt="Sunset in the mountains">
+    <img class="w-full" src="images/children-puppet-show.webp" alt="Sunset in the mountains">
     <div class="px-6 py-4">
         <!-- Title -->
         <div class="event-title font-bold text-xl mb-2">Children’s Storytelling & Puppet Show</div>
@@ -258,7 +588,7 @@ const events = [
             <!-- Date -->
             <p class="mb-2">
                 <i class="fa-solid fa-clock pr-2"></i>
-                <time datetime="2025-05-13">June 28, 2025</time>
+                <time datetime="2025-06-28">June 28, 2025</time>
                 <span class="event-remian-day text-green-500 font-semibold ml-5"> In 1 month </span>
             </p>
             <!-- Address -->
@@ -502,7 +832,7 @@ const events = [
 /* Event - 10
  < !--Event 10 -- >
 <div class="max-w-xs md:max-w-sm rounded overflow-hidden shadow-2xl m-auto mt-5">
-    <img class="w-full" src="images/women-business-forum.png" alt="Sunset in the mountains">
+    <img class="w-full" src="images/women-business-forum.webp" alt="Sunset in the mountains">
     <div class="px-6 py-4">
         <!-- Title -->
         <div class="event-title font-bold text-xl mb-2">Women in Business Leadership Forum</div>
@@ -548,5 +878,5 @@ const events = [
         </button>
     </p>
 </div> <!--Event 10 -- >
-*/ 
+*/
 
