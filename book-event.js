b1e44548e98a4e8f7d0fe6c1f22c5bfaa6badd77
Amiir25 <amirsadik636@gmail.com>
2025-05-18 20:06:42 +0300

    Update book-event.js

--- book-event.js
+++ book-event.js
@@ -1,10 +1,16 @@
-function showSelectedEvent(eventId) {
-    let selectedEvent = events.find(event => event.id === eventId);
-    if (!selectedEvent) return;
+document.addEventListener('DOMContentLoaded', function () {
+    // alert('On the door')
+    function showSelectedEvent(eventId) {
+        let selectedEvent = events.find(event => event.id === eventId);
+        if (!selectedEvent) return;
 
-    alert(selectedEvent.title);
-    let selectedEventTitle = document.querySelector('#selected-event-title').getAttribute('id');
-    alert(selectedEventTitle);
-}
-// document.querySelector('#selected-event-title').textContent = 'Event Title';
-
+        // let backGroundImage = `background-image: url('${clickedEvent.image}');`;
+        // document.querySelector('#selected-event').setAttribute('style', `${backGroundImage}`);
+        document.querySelector('#selected-event-title').textContent = selectedEvent.title;
+        document.querySelector('#selected-event-date').textContent = selectedEvent.date;
+        document.querySelector('#selected-event-price').textContent = selectedEvent.price;
+        document.querySelector('#selected-event-age').textContent = selectedEvent.age;
+    }
+    const eventId = parseInt(sessionStorage.getItem('selectedEventId'), 10);
+    showSelectedEvent(eventId);
+})
